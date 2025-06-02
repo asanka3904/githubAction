c34a765bf2d6acfc86dc7de7a882dc027007f886
Asanka <apsolution001@outlook.com>
2025-06-02 13:58:47 +0530

    update controller

--- src/controllers/mainController.ts
+++ src/controllers/mainController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
 
 const index=(req:Request,res:Response)=>{
-  res.json("hello from server,github CI/CD");
+  res.json("hello from server,CI/CD with github actions");
 }
 
 export {index}
